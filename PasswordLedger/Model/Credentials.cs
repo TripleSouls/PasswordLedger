6594156dc940d443c91eb45cf936e7a0e756095c
Efe <61231517+aefecoban@users.noreply.github.com>
2023-05-14 01:35:05 +0300

    Create Credentials.cs

---
+++ PasswordLedger/Model/Credentials.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordLedger.Model
+{
+    class Credentials
+    {
+        public string Title     = "";
+        public string Username  = "";
+        public string Password  = "";
+        public string Extended  = "";
+    
+        public Credentials() {
+            Title = Username = Password = Extended = "";
+        }
+
+        public Credentials(string title = "", string username = "", string password = "", string extended = "") {
+            Title       = title;
+            Username    = username;
+            Password    = password;
+            Extended    = extended;
+        }
+
+        public void Setter(string? title = null, string? username = null, string? password = null, string? extended = null)
+        {
+            if(title    != null) Title      = title;
+            if(username != null) Username   = username;
+            if(password != null) Password   = password;
+            if(extended != null) Extended   = extended;
+        }
+
+    }
+}
