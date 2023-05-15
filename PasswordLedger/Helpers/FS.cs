913f6080e8b03dbed17798c2918c2d996af7cdd0
Efe <61231517+aefecoban@users.noreply.github.com>
2023-05-15 03:07:42 +0300

    Update FS.cs

--- PasswordLedger/Helpers/FS.cs
+++ PasswordLedger/Helpers/FS.cs
@@ -1,10 +1,11 @@
+using PasswordLedger.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PasswordLedger.Utils
+namespace PasswordLedger.Helpers
 {
     class FS
     {
@@ -58,6 +59,42 @@ namespace PasswordLedger.Utils
             this.isLastQueryFailed = true;
             this.lastErrorMSG = ex.Message;
             this.lastException = ex;
+        }
+
+        public static OperationResult CreateFolderIfNotExists(string path)
+        {
+            try
+            {
+                if (Directory.Exists(path)) { }
+                else
+                {
+                    Directory.CreateDirectory(path);
+                }
+                return new OperationResult() { Success = true };
+            }catch(Exception ex) {
+                return new OperationResult() { Success = false, FailureSource = ex.Source, FailureMessage = ex.Message };
+            }
+        }
+
+        public static OperationResult CreateFileIfNotExists(string path, string data = "")
+        {
+            try
+            {
+                if (File.Exists(path)) { }
+                else
+                {
+                    FileStream filestream = File.Create(path);
+                    StreamWriter sw = new StreamWriter(filestream);
+                    sw.Write(data);
+                    sw.Close();
+                    filestream.Close();
+                }
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception ex)
+            {
+                return new OperationResult() { Success = false, FailureSource = ex.Source, FailureMessage = ex.Message };
+            }
         }
 
     }
