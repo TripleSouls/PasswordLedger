f9dbab9de8315f3177c1fcf4fdf397743f5d98b7
Efe <61231517+aefecoban@users.noreply.github.com>
2023-05-15 03:06:40 +0300

    Create Bootstrap.cs

---
+++ PasswordLedger/Utils/Bootstrap.cs
@@ -0,0 +1,47 @@
+using PasswordLedger.Helpers;
+using PasswordLedger.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordLedger.Utils
+{
+    internal class Bootstrap
+    {
+
+        string path = "";
+        string ledgerFileName = "ledger.ledger";
+        string ledgerFile = "";
+
+        public Bootstrap(string path, string ledgerFileName)
+        {
+            this.path = path;
+            this.ledgerFileName = ledgerFileName;
+            this.ledgerFile = Path.Combine(path, ledgerFileName);
+
+        }
+
+        public void Start()
+        {
+            try
+            {
+                OperationResult or;
+                or = FS.CreateFolderIfNotExists(path);
+                if (!or.Success) { Console.WriteLine("Error:\n" + or.FailureSource + "\n" + or.FailureMessage); throw new Exception(or.FailureMessage);  }
+                
+                or = FS.CreateFileIfNotExists(this.ledgerFile, "{ \"vals\" : []}");
+                if (!or.Success) { Console.WriteLine("Error:\n" + or.FailureSource + "\n" + or.FailureMessage); throw new Exception(or.FailureMessage); }
+
+            }
+            catch (Exception ex) { Console.WriteLine(); }
+        }
+
+        public List<Credential> GetCredentials()
+        {
+            FS fs = new FS(this.ledgerFile);
+            return ModelConverter.JSONToListCredential(fs.ReadFile());
+        }
+    }
+}
