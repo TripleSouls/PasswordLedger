1a8072a53b0088770e39c17ea79d8b46c5201e6d
Efe <61231517+aefecoban@users.noreply.github.com>
2023-05-15 03:07:24 +0300

    Create OperationResult.cs

---
+++ PasswordLedger/Model/OperationResult.cs
@@ -0,0 +1,9 @@
+namespace PasswordLedger.Model
+{
+    class OperationResult
+    {
+        public bool Success;
+        public string FailureSource = "";
+        public string FailureMessage = "";
+    }
+}
