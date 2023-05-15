c87c85ccc170fa5e8ab4ca6b95e2147e8a5e3bc3
Efe <61231517+aefecoban@users.noreply.github.com>
2023-05-15 03:07:57 +0300

    Update Program.cs

--- PasswordLedger/Program.cs
+++ PasswordLedger/Program.cs
@@ -1,9 +1,17 @@
+using PasswordLedger.Model;
 using PasswordLedger.Utils;
 
 public class Program
 {
     public static void Main()
     {
-    
+        Bootstrap bootstrap = new Bootstrap(Functions.GetExePath(), "ledger.ledger");
+        bootstrap.Start();
+
+        Credentials credentials = new Credentials();
+        credentials.SetList(bootstrap.GetCredentials());
+
+        credentials.credentials.ForEach(x =>  Console.WriteLine(x.Title + " " + x.Username + " -> " + x.Password));
+
     }
 }
